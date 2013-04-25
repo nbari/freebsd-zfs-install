aaa8458d6ebf657597272888b638e2c53cd66383
Florent <florent@peterschmitt.fr>
2013-04-25 17:17:04 +0200

    raz geli

--- 9.1-RELEASE/zfs.sh
+++ 9.1-RELEASE/zfs.sh
@@ -23,49 +23,19 @@ gpart create -s gpt $ada
 echo "add"
 gpart add -s 128 -t freebsd-boot -l boot $ada
 gpart add -s $swap_space -t freebsd-swap -l swap $ada
-# if crypt -> ada0p3 will be unencrypted boot and ada0p4 encrypted root
-if [ "$1" = "--crypt" ]; then
-	gpart add -t freebsd-zfs -l zfs-boot -s 2G $ada
-fi
-# In all cases, create the ZFS root volume to take left disk space
 gpart add -t freebsd-zfs -l zfs-root $ada
 
 echo "DD ZERO"
 # Zero ZFS sectors
 dd if=/dev/zero of=/dev/${ada}p3 count=560 bs=512
-if [ "$1" = "--crypt" ]; then
-	dd if=/dev/zero of=/dev/${ada}p4 count=560 bs=512
-fi
 
 echo "BOOTCODE"
 # Put bootcode in freebsd-boot partition
 gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 $ada
 
-
-
 # Let start interesting things :)
-
-if [ "$1" = "--crypt" ]; then
-	kldload geom_eli 2> /dev/null
-
-	echo "ZFS ZBOOT"
-	mkdir /tmp/zboot
-	zpool create -f -m none -o altroot=/tmp/zboot zboot gpt/zfs-boot || exit 1
-
-	mkdir -p /tmp/zboot/boot || exit 1
-	zfs set mountpoint=/boot zboot
-	zfs mount zboot
-	# Create geli key, which will be used to encrypt disk
-	dd if=/dev/random of=/tmp/zboot/boot/${ada}p4.key bs=4096 count=1
-	# Initialize disk with geli. /dev/*.eli entries are unlocked disks
-	geli init -b -K /tmp/zboot/boot/${ada}p4.key -l 256 -s 4096 -e AES-XTS /dev/${ada}p4 || exit 1
-	geli attach -k /tmp/zboot/boot/${ada}p4.key /dev/${ada}p4
-
-	zpool create -f -m none -o altroot=/mnt tank /dev/${ada}p4.eli
-else
-	echo "ZPOOL TANK"
-	zpool create -f -m none -o altroot=/mnt -o cachefile=/tmp/zpool.cache tank gpt/zfs-root
-fi
+echo "ZPOOL TANK"
+zpool create -f -m none -o altroot=/mnt -o cachefile=/tmp/zpool.cache tank gpt/zfs-root
 
 echo "ZFS CREATE"
 zfs create -o mountpoint=/ tank/root
@@ -78,23 +48,14 @@ zfs create -o mountpoint=/usr/ports tank/usr/ports
 zfs create -o mountpoint=/home tank/home
 
 echo "BOOT SET"
-if [ "$1" = "--crypt" ]; then
-	zpool set bootfs=zboot zboot
-else
-	zpool set bootfs=tank/root tank
-fi
+zpool set bootfs=tank/root tank
 
 cd /mnt
 
 echo "TAR"
 fetch $url/kernel.txz
 fetch $url/base.txz
-if [ "$1" = "--crypt" ]; then
-	ln -sf /tmp/zboot/boot /mnt/boot
-	tar xJpf kernel.txz -C /tmp/zboot/boot
-else
-	tar xJpf kernel.txz -C /mnt
-fi
+tar xJpf kernel.txz -C /mnt
 tar xJpf base.txz -C /mnt
 
 echo "CONF"
@@ -105,14 +66,6 @@ echo "hostname=\"$hostname\"" >> /mnt/etc/rc.conf
 echo "ifconfig_${ifconfig_if}=\"inet $ifconfig_addr netmask $ifconfig_mask broadcast $ifconfig_brdc\"" >> /mnt/etc/rc.conf
 echo 'sshd_enable="YES"' >> /mnt/etc/rc.conf
 echo '/dev/ada0p2 none swap sw 0 0' > /mnt/etc/fstab
-
-if [ "$1" = "--crypt" ];then
-	echo 'geom_eli_load="YES"' >> /mnt/boot/loader.conf
-	echo "geli_devices=\"${ada}p4\"" >> /mnt/etc/rc.conf
-	echo "geli_${ada}p4_keyfile0_load=\"YES\"" >> /mnt/boot/loader.conf
-	echo "geli_${ada}p4_keyfile0_type=\"${ada}p4:geli_keyfile0\"" >> /mnt/boot/loader.conf
-	echo "geli_${ada}p4_keyfile0_name=\"/boot/${ada}p4.key\"" >> /mnt/boot/loader.conf
-fi
 
 cd /
 
