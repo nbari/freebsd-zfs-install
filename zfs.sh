8c0cec87bb3a4746030233756ebdf225bd77d9e5
Florent Peterschmitt <florent@peterschmitt.fr>
2013-05-04 18:31:50 +0200

    messages, resolv.conf, mdconfig

--- zfs.sh
+++ zfs.sh
@@ -1,41 +1,48 @@
 #!/usr/bin/env sh
+
 ada="ada0"
 swap_space="4G"
-url="ftp://ftp.fr.freebsd.org/pub/FreeBSD/releases/amd64/9.1-RELEASE"
-#url="ftp://ftp.fr.freebsd.org/pub/FreeBSD/snapshots/amd64/amd64/9.1-STABLE"
-echo "ZFS RELATED"
+#keymap="us.iso.acc.kbd"
+#keymap="uk.iso.kbd"
+keymap="fr.iso.acc.kbd"
+hostname="freebsd-zfs"
+nameserver="8.8.8.8"
+
+#url="http://ftp.fr.freebsd.org/pub/FreeBSD/releases/amd64/9.1-RELEASE"
+url="http://ftp.fr.freebsd.org/pub/FreeBSD/snapshots/amd64/amd64/9.1-STABLE"
+#url="http://192.168.0.23/~florent/freebsd-zfs/9.1-RELEASE"
+#url="http://192.168.0.23/~florent/freebsd-zfs/9.1-STABLE"
+#lib32=yes
+#src=yes
+#doc=yes
+
+#export http_proxy="http://proxy:3128"
+
+dest="/mnt"
+taropt="xvJpf"
+
 kldload opensolaris 2> /dev/null
 kldload zfs 2> /dev/null
 
-zpool import -f -o altroot=/mnt tank
+zpool import -f -o altroot=$dest tank
 zpool destroy -f tank
-zpool import -f -o altroot=/mnt zboot
-zpool destroy -f zboot
-
 zpool labelclear /dev/${ada}*
 
-echo "GPART"
 gpart destroy -F $ada
-echo "create"
 gpart create -s gpt $ada
-echo "add"
 gpart add -s 128 -t freebsd-boot -l boot $ada
 gpart add -s $swap_space -t freebsd-swap -l swap $ada
 gpart add -t freebsd-zfs -l zfs-root $ada
 
-echo "DD ZERO"
 # Zero ZFS sectors
 dd if=/dev/zero of=/dev/${ada}p3 count=560 bs=512
 
-echo "BOOTCODE"
 # Put bootcode in freebsd-boot partition
 gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 $ada
 
 # Let start interesting things :)
-echo "ZPOOL TANK"
-zpool create -f -m none -o altroot=/mnt -o cachefile=/tmp/zpool.cache tank gpt/zfs-root
+zpool create -f -m none -o altroot=$dest -o cachefile=/tmp/zpool.cache tank gpt/zfs-root
 
-echo "ZFS CREATE"
 zfs create -o mountpoint=/ tank/root
 zfs create -o mountpoint=/tmp tank/root/tmp
 zfs create -o mountpoint=/var tank/root/var
@@ -45,37 +52,69 @@ zfs create -o mountpoint=/usr/local tank/root/usr/local
 zfs create -o mountpoint=/usr/ports tank/root/usr/ports
 zfs create -o mountpoint=/home tank/root/home
 
-echo "BOOT SET"
 zpool set bootfs=tank/root tank
 
-cd /mnt
+cd $dest
 
-echo "TAR"
-fetch $url/kernel.txz
-fetch $url/base.txz
-fetch $url/src.txz
-fetch $url/lib32.txz
-tar xJpf kernel.txz -C /mnt
-tar xJpf base.txz -C /mnt
-tar xJpf src.txz -C /mnt
-tar xJpf lib32.txz -C /mnt
+mdconfig -a -t malloc -s 1m -u 3
+newfs -O 1 /dev/md3
+mount /dev/md3 /etc
+echo "nameserver $nameserver" > /etc/resolv.conf
 
-echo "CONF"
-echo 'zfs_load="YES"' > /mnt/boot/loader.conf
-echo 'vfs.root.mountfrom="zfs:tank/root"' >> /mnt/boot/loader.conf
-echo 'zfs_enable="YES"' > /mnt/etc/rc.conf
-echo 'sshd_enable="YES"' >> /mnt/etc/rc.conf
-echo '/dev/ada0p2 none swap sw 0 0' > /mnt/etc/fstab
+fetch -a $url/kernel.txz
+tar $taropt kernel.txz -C $dest
+
+if [ ! $? ]; then
+	exit 1
+fi
+
+fetch -a $url/base.txz
+tar $taropt base.txz -C $dest
+if [ ! $? ]; then
+	exit 1
+fi
+
+if [ ! -z $lib32 ]; then
+	fetch -a $url/lib32.txz
+	tar $taropt lib32.txz -C $dest
+	if [ ! $? ]; then
+		exit 1
+	fi
+fi
+if [ ! -z $src ]; then
+	fetch -a $url/src.txz
+	tar $taropt src.txz -C $dest
+	if [ ! $? ]; then
+		exit 1
+	fi
+fi
+if [ ! -z $doc ]; then
+	fetch -a $url/doc.txz
+	tar $taropt doc.txz -C $dest
+	if [ ! $? ]; then
+		exit 1
+	fi
+fi
+umount /etc
+
+echo 'zfs_load="YES"' > $dest/boot/loader.conf
+echo 'vfs.root.mountfrom="zfs:tank/root"' >> $dest/boot/loader.conf
+echo 'zfs_enable="YES"' > $dest/etc/rc.conf
+echo "keymap=\"$keymap\"" >> $dest/etc/rc.conf
+echo "hostname=\"$hostname\"" >> $dest/etc/rc.conf
+echo 'sshd_enable="YES"' >> $dest/etc/rc.conf
+echo '/dev/ada0p2 none swap sw 0 0' > $dest/etc/fstab
 
 cd /
 
-echo "EXPORT"
 zpool export tank
-zpool import -o cachefile=/tmp/zpool.cache -o altroot=/mnt tank
-cp /tmp/zpool.cache /mnt/boot/zfs/
+zpool import -o cachefile=/tmp/zpool.cache -o altroot=$dest tank
+cp /tmp/zpool.cache $dest/boot/zfs/
 
-echo "You just have been chrooted into your fresh installation."
+echo "You've just been chrooted into your fresh installation."
 echo "passwd root
 hostname=\"yourhostname\" in rc.conf and make alias in /etc/hosts.
-Add users, import conf file from anywhere... do what you want and reboot."
-chroot /mnt /bin/tcsh
+Add users, import conf file from anywhere... do what you want and reboot.
+Do \"chroot $dest /bin/tcsh\" if you want to go in ;)
+Be careful, you are in a C-shell."
+chroot $dest /bin/tcsh
